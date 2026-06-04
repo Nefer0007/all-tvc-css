6250c8625e48deb042f78c3098f6e6cbafd7b231
Nefer0007 <szerepjate34@gmail.com>
2026-06-04 19:50:59 +0200

    Create montazsocskakepek.cc

---
+++ montazsocskakepek.cc
@@ -0,0 +1,28 @@
+.kepmontazsokhattere {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: var(--reagbg);
+    width: 614px;
+    border-radius: 3px;
+}.kepekbg {
+    display: flex;
+    justify-content: center;
+    margin: 2px;
+}img.montazskepelso {
+    width: 187px;
+    height: 178px;
+    margin: 1px 1px 1px 4px;
+    background-color: var(--regocskaabgg);
+    border: 2px solid var(--regocskaabgg);
+    box-shadow: 0 0 2px #0a0a0a, 0 0 10px var(--regocskaabgg) inset;
+    padding: 2px;
+}img.montazskepmasodik {
+    width: 391px;
+    height: 178px;
+    margin: 1px 1px 1px 4px;
+    padding: 2px;
+    background-color: var(--regocskaabgg);
+    border: 2px solid var(--regocskaabgg);
+    box-shadow: 0 0 2px #0a0a0a, 0 0 10px var(--regocskaabgg) inset;
+}
